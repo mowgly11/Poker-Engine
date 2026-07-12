d348f285e704fa68ab336d29d2b79cca6160950b
John <83482871+mowgly11@users.noreply.github.com>
2026-07-13 00:42:53 +0100

    feat: add new display method to expose the table, startGame and placeCard methods to use for a game

--- modules/table.ts
+++ modules/table.ts
@@ -1,5 +1,8 @@
 import type { Card } from "../types/deck";
 import type { Player } from "../types/table";
+import Deck from "./deck";
+
+const deck = new Deck();
 
 class Table {
     private players: Player[] = []; // name, money, cards
@@ -9,16 +12,33 @@ class Table {
     /**
      * 
      * @param players list of players names
-     * @param initialMoney 
-     * @param dealer 
+     * @param initialMoney initial amount of money that all players will have at the beginning of the game
      */
 
-    constructor(playersNames: string[], initialMoney: number = -1, dealer: string) {
+    constructor(playersNames: string[], initialMoney: number = -1) {
         if(playersNames.length < 2) throw new Error("Players should be more then 2");
         this.players = [...playersNames.map(p => {
             return { username: p, money: initialMoney, currentCards: [] }
         })];
         this.dealer = this.players[0]!.username;
+    }
+
+    public displayTable() {
+        return {
+            players: this.players,
+            dealer: this.dealer,
+            table: this.table
+        }
+    }
+
+    public startGame() {
+        this.players.forEach(p => {
+            p.currentCards?.push(...deck.takeCard(2));
+        });
+    }
+
+    public placeCard(amount: number = 1) {
+        this.table.push(...deck.takeCard(amount));
     }
 }
 
