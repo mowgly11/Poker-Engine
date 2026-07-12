f38041f5cec0f592b45ab1a2016531a4294046f3
John <83482871+mowgly11@users.noreply.github.com>
2026-07-13 00:44:22 +0100

    fix: added constant for cards to prevent redundancy

--- modules/deck.ts
+++ modules/deck.ts
@@ -1,12 +1,14 @@
 import type { Card } from "../types/deck";
 
-class Deck {
-    private cards: Card[] = [
+const CARDS: Card[] = [
         "clubs:1", "clubs:2", "clubs:3", "clubs:4", "clubs:5", "clubs:6", "clubs:7", "clubs:8", "clubs:9", "clubs:10", "clubs:11", "clubs:12", "clubs:13",
         "spades:1", "spades:2", "spades:3", "spades:4", "spades:5", "spades:6", "spades:7", "spades:8", "spades:9", "spades:10", "spades:11", "spades:12", "spades:13",
         "diamonds:1", "diamonds:2", "diamonds:3", "diamonds:4", "diamonds:5", "diamonds:6", "diamonds:7", "diamonds:8", "diamonds:9", "diamonds:10", "diamonds:11", "diamonds:12", "diamonds:13",
         "hearts:1", "hearts:2", "hearts:3", "hearts:4", "hearts:5", "hearts:6", "hearts:7", "hearts:8", "hearts:9", "hearts:10", "hearts:11", "hearts:12", "hearts:13",
-    ];
+    ]
+
+class Deck {
+    private cards: Card[] = [...CARDS];
     private size = 52;
 
     constructor() {
@@ -38,12 +40,7 @@ class Deck {
     }
 
     public reset() {
-        this.cards = [
-            "clubs:1", "clubs:2", "clubs:3", "clubs:4", "clubs:5", "clubs:6", "clubs:7", "clubs:8", "clubs:9", "clubs:10", "clubs:11", "clubs:12", "clubs:13",
-            "spades:1", "spades:2", "spades:3", "spades:4", "spades:5", "spades:6", "spades:7", "spades:8", "spades:9", "spades:10", "spades:11", "spades:12", "spades:13",
-            "diamonds:1", "diamonds:2", "diamonds:3", "diamonds:4", "diamonds:5", "diamonds:6", "diamonds:7", "diamonds:8", "diamonds:9", "diamonds:10", "diamonds:11", "diamonds:12", "diamonds:13",
-            "hearts:1", "hearts:2", "hearts:3", "hearts:4", "hearts:5", "hearts:6", "hearts:7", "hearts:8", "hearts:9", "hearts:10", "hearts:11", "hearts:12", "hearts:13"
-        ];
+        this.cards = [...CARDS];
         this.shuffle();
     }
 }
